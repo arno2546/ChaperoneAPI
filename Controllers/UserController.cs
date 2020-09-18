7a2f099cbb5cc22d49468cd7dec4dcaff5662df0
arno2546 <arno.ahmed2546@gmail.com>
2020-09-18 16:49:09 +0600

    UserController done

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Chaperone_API.Repository;
+﻿using Chaperone_API.Models;
+using Chaperone_API.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,44 @@ namespace Chaperone_API.Controllers
     public class UserController : ApiController
     {
         UserRepository userRepo = new UserRepository();
+
         [Route("")]
         public IHttpActionResult Get()
         {
             return Ok(userRepo.GetAll());
+        }
+        [Route("{id}", Name ="GetUserById")]
+        public IHttpActionResult Get(int id)
+        {
+            User u = userRepo.GetById(id);
+            if (u == null)
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            return Ok(u);
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            userRepo.Delete(id);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [Route("")]
+        public IHttpActionResult Post(User u)
+        {
+            userRepo.Insert(u);
+            string url = Url.Link("GetUserById", new { id = u.UserId });
+            return Created(url, u);
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult Put([FromBody]User u,[FromUri]int id)
+        {
+            u.UserId = id;
+            userRepo.Edit(u);
+            return Ok(u);
         }
     }
 }
