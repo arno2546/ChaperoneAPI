c4750db23ea7906bc4ea2a02ed993e81d1621d8a
arno2546 <arno.ahmed2546@gmail.com>
2020-09-21 17:44:54 +0600

    implementerd login

---
+++ Controllers/LogInController.cs
@@ -0,0 +1,30 @@
+﻿using Chaperone_API.Models;
+using Chaperone_API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chaperone_API.Controllers
+{
+    [RoutePrefix("api/login")]
+    public class LogInController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Post(LogIn l)
+        {
+            LogIn valUser = new LogIn();
+            UserRepository userRepo = new UserRepository();
+            User u = userRepo.GetAll().Where(x => x.Email == l.Email && x.Password == l.Password).FirstOrDefault();
+            if (u!= null)
+            {
+                valUser.UserType = u.UserType;
+                valUser.Email = u.Email;
+                return Ok(valUser);
+            }
+            return StatusCode(HttpStatusCode.NotFound);
+        }
+    }
+}
