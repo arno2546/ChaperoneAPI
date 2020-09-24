ef94b1b63f14ec26756f231639cd452c0a53931a
arno2546 <arno.ahmed2546@gmail.com>
2020-09-24 20:39:35 +0600

    userRatio implemented

---
+++ Controllers/UserRatioController.cs
@@ -0,0 +1,29 @@
+﻿using Chaperone_API.Models;
+using Chaperone_API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chaperone_API.Controllers
+{
+    [RoutePrefix("api/userRatio")]
+    public class UserRatioController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            UserRepository userRepo = new UserRepository();
+            int Guide = userRepo.GetAll().Where(x => x.UserType == "Guide").Count();
+            int Admin = userRepo.GetAll().Where(x => x.UserType == "Admin").Count();
+            int Tourist = userRepo.GetAll().Where(x => x.UserType == "Gen").Count();
+            UserRatio userRatio = new UserRatio();
+            userRatio.Admin = Admin;
+            userRatio.Guide = Guide;
+            userRatio.Tourist = Tourist;
+            return Ok(userRatio);
+        }
+    }
+}
---
+++ Models/UserRatio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chaperone_API.Models
+{
+    public class UserRatio
+    {
+        public int Admin { get; set; }
+        public int Tourist { get; set; }
+        public int Guide { get; set; }
+    }
+}
