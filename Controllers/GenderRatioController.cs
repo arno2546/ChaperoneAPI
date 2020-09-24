d04ad038e57fb81df686d586aa1f71aaa01f2391
arno2546 <arno.ahmed2546@gmail.com>
2020-09-24 19:31:50 +0600

    genderRatio implemented

---
+++ Controllers/GenderRatioController.cs
@@ -0,0 +1,27 @@
+﻿using Chaperone_API.Models;
+using Chaperone_API.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chaperone_API.Controllers
+{
+    [RoutePrefix("api/genderRatio")]
+    public class GenderRatioController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            UserRepository userRepo = new UserRepository();
+            int male = userRepo.GetAll().Where(x =>x.UserType=="Guide" && x.Gender == "Male").Count();
+            int female = userRepo.GetAll().Where(x => x.UserType == "Guide" && x.Gender == "Female").Count();                
+            GenderRatio genRatio = new GenderRatio();
+            genRatio.MaleRatio = male;
+            genRatio.FemaleRatio = female;
+            return Ok(genRatio);
+        }
+    }
+}
---
+++ Models/GenderRatio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chaperone_API.Models
+{
+    public class GenderRatio
+    {
+        public float MaleRatio { get; set; }
+        public float FemaleRatio { get; set; }
+    }
+}
