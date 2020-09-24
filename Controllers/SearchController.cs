801829546a197967cb6268bb63a7a709f6ebe0f0
arno2546 <arno.ahmed2546@gmail.com>
2020-09-24 22:51:45 +0600

    fixed search

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -17,6 +17,90 @@ namespace Chaperone_API.Controllers
         {
             UserRepository userRepo = new UserRepository();
             List<User> FilterdUsers = userRepo.GetAll().Where(x => x.UserType == "Guide" && x.Location.Contains(s.SearchString.ToString())).ToList();
+            if (s.Male)
+            {
+                List<User> user = new List<User>();
+                foreach (User u in FilterdUsers)
+                {
+                    if (u.Gender == "Male")
+                    {
+                        user.Add(u);
+                    }
+                }
+                FilterdUsers = user;
+            }
+            if (s.Female)
+            {
+                List<User> user = new List<User>();
+                foreach (User u in FilterdUsers)
+                {
+                    if (u.Gender=="Female")
+                    {
+                        user.Add(u);
+                    }
+                }
+                FilterdUsers = user;
+            }
+            if (s.Culture)
+            {
+                List<User> user = new List<User>();
+                foreach (User u in FilterdUsers)
+                {
+                    if (u.Culture)
+                    {
+                        user.Add(u);
+                    }
+                }
+                FilterdUsers = user;
+            }
+            if (s.Festival)
+            {
+                List<User> user = new List<User>();
+                foreach (User u in FilterdUsers)
+                {
+                    if (u.Festival)
+                    {
+                        user.Add(u);
+                    }
+                }
+                FilterdUsers = user;
+            }
+            if (s.NightLife)
+            {
+                List<User> user = new List<User>();
+                foreach (User u in FilterdUsers)
+                {
+                    if (u.NightLife)
+                    {
+                        user.Add(u);
+                    }
+                }
+                FilterdUsers = user;
+            }
+            if (s.Food)
+            {
+                List<User> user = new List<User>();
+                foreach (User u in FilterdUsers)
+                {
+                    if (u.Food)
+                    {
+                        user.Add(u);
+                    }
+                }
+                FilterdUsers = user;
+            }
+            if (s.Sports)
+            {
+                List<User> user = new List<User>();
+                foreach (User u in FilterdUsers)
+                {
+                    if (u.Sports)
+                    {
+                        user.Add(u);
+                    }
+                }
+                FilterdUsers = user;
+            }
             return Ok(FilterdUsers);
         }
     //    public IHttpActionResult Post(Search s)
